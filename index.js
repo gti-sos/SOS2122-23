72a47ec9ef31407c61ace66d757bbfd102db5d7c
albmarmar6 <albertomartinmartin201@gmail.com>
2022-03-13 19:51:12 +0100

    heroku

---
+++ index.js
@@ -0,0 +1,23 @@
+const cool = require("cool-ascii-faces");
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use("/", express.static('public'))
+
+app.get("/faces", (req,res)=>{
+    console.log("Requested /faces route");
+    res.send("<html><body><h1>"+cool()+"</h1></body></html>")
+});
+
+app.get("/time", (req,res)=>{
+    console.log("Requested /time route");
+    res.send("<html><body><h1>"+new Date()+"</h1></body></html>")
+});
+
+
+app.listen(port, () => {
+    console.log(`Server ready at port ${port}`);
+});
+
