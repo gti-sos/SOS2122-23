d6398603a4d442941a62f53deb1e3880860f2af1
albmarmar6 <albertomartinmartin201@gmail.com>
2022-03-20 12:20:15 +0100

    l06

--- index.js
+++ index.js
@@ -1,21 +1,69 @@
 const cool = require("cool-ascii-faces");
 const express = require("express");
+const bodyParser = require("body-parser");
+const res = require("express/lib/response");
 
 const app = express();
+
+const API_DOC_PORTAL = "https://documenter.getpostman.com/view/19586040/UVsPQkGD";
+
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 8080;
 
-app.use("/", express.static('public'))
+const BASE_API_URL = "/api/v1"; 
 
-app.get("/faces", (req,res)=>{
-    console.log("Requested /faces route");
-    res.send("<html><body><h1>"+cool()+"</h1></body></html>")
+var contacts = [
+    {
+        name:"peter",
+        phone:123456
+    },
+    {
+        name:"paul",
+        phone:56789
+    }
+];
+
+
+app.get(BASE_API_URL+"/docs",(req,res)=>{
+    res.redirect(API_DOC_PORTAL);
 });
 
-app.get("/time", (req,res)=>{
-    console.log("Requested /time route");
-    res.send("<html><body><h1>"+new Date()+"</h1></body></html>")
+app.get(BASE_API_URL+"/contacts",(req,res)=>{
+    res.send(JSON.stringify(contacts,null,2));
 });
 
+app.get(BASE_API_URL+"/contacts/:name",(req,res)=>{
+    
+    var contactName = req.params.name;
+    filteredContacts = contacts.filter((contact)=>{
+        return (contact.name == contactName);
+    });
+
+    if(filteredContacts == 0){
+        res.sendStatus(404,"NOT FOUND");
+    }else{
+        res.send(JSON.stringify(filteredContacts[0],null,2));
+    }
+});
+
+app.post(BASE_API_URL+"/contacts",(req,res)=>{
+    contacts.push(req.body);
+    res.sendStatus(201,"Created");
+});
+
+app.delete(BASE_API_URL+"/contacts",(req,res)=>{
+    contacts = [];
+    res.sendStatus(200,"Ok");
+});
+
+app.delete(BASE_API_URL+"/contacts/:name",(req,res)=>{
+    var contactName = req.params.name;
+    contacts = contacts.filter((contact)=>{
+        return (contact.name != contactName);
+    });
+    res.sendStatus(200,"OK");
+});
 
 app.listen(port, () => {
     console.log(`Server ready at port ${port}`);
