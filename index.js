3d12b7399d82589677b2a516d6bcd40ba61dabf2
Antonio Saborido <antonio.saborido01@gmail.com>
2022-03-21 18:07:05 +0100

    Index.html update

--- index.js
+++ index.js
@@ -13,57 +13,17 @@ const port = process.env.PORT || 8080;
 
 const BASE_API_URL = "/api/v1"; 
 
-var contacts = [
-    {
-        name:"peter",
-        phone:123456
-    },
-    {
-        name:"paul",
-        phone:56789
-    }
-];
+app.use("/",express.static('public'));
 
+app.get("/cool", (req,res) => {
+    console.log("Requested / route");
+    res.send(`<html>
+                <body>
+                    <h1>`+cool()+`</h1>
+                </body>
+            </html>`);
+})
 
-app.get(BASE_API_URL+"/docs",(req,res)=>{
-    res.redirect(API_DOC_PORTAL);
-});
-
-app.get(BASE_API_URL+"/contacts",(req,res)=>{
-    res.send(JSON.stringify(contacts,null,2));
-});
-
-app.get(BASE_API_URL+"/contacts/:name",(req,res)=>{
-    
-    var contactName = req.params.name;
-    filteredContacts = contacts.filter((contact)=>{
-        return (contact.name == contactName);
-    });
-
-    if(filteredContacts == 0){
-        res.sendStatus(404,"NOT FOUND");
-    }else{
-        res.send(JSON.stringify(filteredContacts[0],null,2));
-    }
-});
-
-app.post(BASE_API_URL+"/contacts",(req,res)=>{
-    contacts.push(req.body);
-    res.sendStatus(201,"Created");
-});
-
-app.delete(BASE_API_URL+"/contacts",(req,res)=>{
-    contacts = [];
-    res.sendStatus(200,"Ok");
-});
-
-app.delete(BASE_API_URL+"/contacts/:name",(req,res)=>{
-    var contactName = req.params.name;
-    contacts = contacts.filter((contact)=>{
-        return (contact.name != contactName);
-    });
-    res.sendStatus(200,"OK");
-});
 
 app.listen(port, () => {
     console.log(`Server ready at port ${port}`);
@@ -96,7 +56,7 @@ app.post(BASE_API_URL+"/premier-league-stats",(req,res)=>{
     res.sendStatus(201,"Created");
 });
 
-//#### API TENNIS ###
+//#### API TENNIS - ANTONIO SABORIDO CAMPOS ###
 
 var tennis = [
     {
@@ -172,7 +132,7 @@ var nba = [
 ];
 
 app.get(BASE_API_URL+"/nba-stats",(req,res)=>{
-    res.send(JSON.stringify(nba,null,2));
+    res.send(JSON.stringify(stats,null,2));
 });
 
 app.post(BASE_API_URL+"/nba-stats",(req,res)=>{
