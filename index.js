fe2a13ed24d2f67c9e3e779fdbc6edbc9079edf7
ferparbel <ferparbel@alum.us.es>
2022-03-29 20:11:41 +0200

    eliminar nba-stats

--- index.js
+++ index.js
@@ -34,6 +34,38 @@ var tennis = require("./tennis");
 tennis.register(app);
 
 //API Fernando Pardo Beltrán(nba-stats)
-var nbaStats = require("./nba-stats");
-nbaStats.register(app);
 
+var nbaStats = [
+    {
+        country:"usa",
+        year:2013,
+        mostpoints:2280,
+        fieldgoals:731,
+        efficiency:30.4
+    },
+    {
+        country:"usa",
+        year:2014,
+        mostpoints:2593,
+        fieldgoals:849,
+        efficiency:31.8
+    },
+    {
+        country:"usa",
+        year:2015,
+        mostpoints:2217,
+        fieldgoals:647,
+        efficiency:27.2
+    }
+];
+
+//GET todos los recursos
+app.get(BASE_API_URL,(req,res)=>{
+    res.send(JSON.stringify(nbaStats,null,2));
+});
+
+//POST de un recurso
+app.post(BASE_API_URL,(req,res)=>{
+    nbaStats.push(req.body);
+    res.sendStatus(201,"Created");
+});
