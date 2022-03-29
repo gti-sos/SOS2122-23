a23f25cd7190ec7e120b4e837491b3dc04c167ae
ferparbel <ferparbel@alum.us.es>
2022-03-29 20:41:41 +0200

    correccion fallos fernando v4

--- nba-stats.js
+++ nba-stats.js
@@ -1,45 +1,42 @@
-const BASE_API_URL = "/api/v1/nba-stats";
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
+module.exports.register = (app) => {
+    const BASE_API_URL = "/api/v1/nba-stats";
+    const path = require("path");
+    const bodyParser = require("body-parser");
+    app.use(bodyParser.json());
     
+    var nbaStats = [
+        {
+            country:"usa",
+            year:2013,
+            mostpoints:2280,
+            fieldgoals:731,
+            efficiency:30.4
+        },
+        {
+            country:"usa",
+            year:2014,
+            mostpoints:2593,
+            fieldgoals:849,
+            efficiency:31.8
+        },
+        {
+            country:"usa",
+            year:2015,
+            mostpoints:2217,
+            fieldgoals:647,
+            efficiency:27.2
+        }
+    ];
 
-var nbaStats = [
-    {
-        country:"usa",
-        year:2013,
-        mostpoints:2280,
-        fieldgoals:731,
-        efficiency:30.4
-    },
-    {
-        country:"usa",
-        year:2014,
-        mostpoints:2593,
-        fieldgoals:849,
-        efficiency:31.8
-    },
-    {
-        country:"usa",
-        year:2015,
-        mostpoints:2217,
-        fieldgoals:647,
-        efficiency:27.2
-    }
-]
+        //GET todos los recursos
+        app.get(BASE_API_URL,(req,res)=>{
+            res.send(JSON.stringify(nbaStats,null,2));
+        });
 
-
-module.exports.register = (app) =>{
-
-    //GET todos los recursos
-    app.get(BASE_API_URL,(req,res)=>{
-        res.send(JSON.stringify(nbaStats,null,2));
-    });
-
-    //POST de un recurso
-    app.post(BASE_API_URL,(req,res)=>{
-        nbaStats.push(req.body);
-    res.sendStatus(201,"Created");
-    });
+        //POST de un recurso
+        app.post(BASE_API_URL,(req,res)=>{
+            nbaStats.push(req.body);
+            res.sendStatus(201,"Created");
+        });
 }
 
