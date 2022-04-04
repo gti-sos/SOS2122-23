f346c41c77a45d043d681558a728ce438f4d480b
ferparbel <ferparbel@alum.us.es>
2022-04-04 13:47:20 +0200

    delete paginación

--- src/back/tennis/tennis.js
+++ src/back/tennis/tennis.js
@@ -94,7 +94,7 @@ module.exports = (app) => {
 
     // GET - RESOURCE
     
-    /*app.get(BASE_API_URL + API_NAME,(req,res)=>{
+    app.get(BASE_API_URL + API_NAME,(req,res)=>{
         db.find({}, function(err,docs){
             res.send(JSON.stringify(docs.map((c)=>{
                 return {country : c.country, year : c.year, most_grand_slam : c.most_grand_slam, masters_finals : c.masters_finals, olympic_gold_medals : c.olympic_gold_medals};
@@ -104,58 +104,8 @@ module.exports = (app) => {
         
 
     });
-    */
+    
 
-    app.get(BASE_API_URL + OWN_API_URL, (req,res)=>{ 
-        var query = req.query;
-        dbquery = {};
-        console.log("Peticion GET");
-        console.log(query.year);
-        var limit = Number.MAX_SAFE_INTEGER;
-        var offset = 0;
-        if(query.offset){
-            offset = parseInt(query.offset);
-            console.log(offset);
-            delete query.offset;
-        }
-        if(query.limit){
-            limit = parseInt(query.limit);
-            delete query.limit;
-        }
-        if(query.year){
-            dbquery['year'] = parseInt(query.year);
-            console.log(offset);
-        }
-        if(query.most_grand_slam){
-            dbquery['most_grand_slam'] = parseFloat(query.most_grand_slam);
-        }
-        if(query.masters_finals){
-            dbquery['masters_finals'] = parseFloat(query.masters_finals);
-        }
-        if(query.olympic_gold_medals){
-            dbquery['olympic_gold_medals'] = parseFloat(query.olympic_gold_medals);
-        }
-        
-        console.log(dbquery);
-        db.find(dbquery).skip(offset).limit(limit).exec((err,docs) =>{
-            console.log(docs);
-            if(err){
-                res.sendStatus(500);
-            }
-            else{
-                if(docs == 0){
-                    res.sendStatus(404);
-                }
-                else{
-                    docs.forEach((data) => {
-                        delete data._id;
-                    });
-                    res.status(200).send(JSON.stringify(docs,null,2));
-                }
-            }
-        })
-        //res.send(JSON.stringify(landusage_stats_copy, null,2)); // devuelve el conjunto 
-    });
 
 /*          ALTERNATIVE GET RESOURCE
     app.get(BASE_API_URL + url_sergio,(req,res)=>{
