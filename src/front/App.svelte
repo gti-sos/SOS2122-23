2016064e226f821152fbb1db8d541cd0508bf23e
Antonio Saborido <antonio.saborido01@gmail.com>
2022-04-11 01:04:09 +0200

    svelte implementation

---
+++ src/front/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Router from "svelte-spa-router";
+	import Home from "./Home.svelte";
+	
+	const routes = {
+
+		"/": Home
+	};
+
+</script>
+
+<main>
+	<Router {routes} />
+</main>
---
+++ src/front/Home.svelte
@@ -0,0 +1,16 @@
+<main>
+    <h1>SOS2122-23</h1>
+
+        <h2>Team</h2>
+        <p>- [Alberto Martín Martín](https://github.com/albmarmar6)</p>
+        <p>- <a href="https://github.com/Nando13-hub">Fernando Pardo Beltrán</a></p>
+        <p>- <a href="https://github.com/Antoniiosc7">[Antonio Saborido Campos]</a></p>
+        <h2>Project description:</h2> Relación de deportes entre fútbol (Premier League), tenis y baloncesto (NBA)
+        <h2>Repository:</h2><a href="https://github.com/gti-sos/SOS2122-23">gti-sos/SOS2122-23</a>
+        <h2>URL:</h2><a href="http://sos2122-23.herokuapp.com"> http://sos2122-23.herokuapp.com</a>
+        <h2>APIS:</h2>
+        <li><a href="https://sos2122-23.herokuapp.com/api/v1/premier-league">https://sos2122-23.herokuapp.com/api/v1/premier-league-stats (developed by Alberto Martin Martin)(https://github.com/albmarmar6)</a></li>
+        <li><a href="https://sos2122-23.herokuapp.com/api/v1/nba-stats">https://sos2122-23.herokuapp.com/api/v1/nba-stats (developed by Fernando Pardo Beltrán)</a></li>
+        <li><a href="https://sos2122-23.herokuapp.com/api/v1/tennis">https://sos2122-23.herokuapp.com/api/v1/tennis (developed by Antonio Saborido Campos)</a></li>
+
+</main>
