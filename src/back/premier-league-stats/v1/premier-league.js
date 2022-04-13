ada89244976efcfbab142bbff2a82d43858d5ccc
albmarmar6 <albertomartinmartin201@gmail.com>
2022-04-13 21:19:14 +0200

    correccion api alberto

---
+++ src/back/premier-league-stats/v1/premier-league.js
@@ -0,0 +1,432 @@
+const bodyParser = require("body-parser");
+const res = require("express/lib/response");
+const BASE_API_URL = "/api/v1/premier-league";
+const pman = "https://documenter.getpostman.com/view/19586040/UVyn2yuS";
+
+//Array de objetos
+
+var stats = [
+    {
+        countries:"England",
+        years:2018,
+        appearences:38,
+        cleanSheets:21,
+        goals:22
+    },
+    {
+        countries:"England",
+        years:2019,
+        appearences:38,
+        cleanSheets:16,
+        goals:21
+    },
+    {
+        countries:"Belgium",
+        years:2015,
+        appearences:38,
+        cleanSheets:11,
+        goals:18
+    },
+    {
+        countries:"Belgium",
+        years:2017,
+        appearences:38,
+        cleanSheets:16,
+        goals:16
+    },
+    {
+        countries:"Spain",
+        years:2013,
+        appearences:37,
+        cleanSheets:12,
+        goals:9
+    },
+    {
+        countries:"Spain",
+        years:2020,
+        appearences:37,
+        cleanSheets:10,
+        goals:7
+    }
+];
+
+
+module.exports.register = (app,db) => {
+
+//Portal de Documentacion
+
+app.get(BASE_API_URL_DEFENSE+"/docs",(req,res)=>
+{
+    res.redirect(pman);
+});
+
+
+
+//Cargar Datos Iniciales
+
+app.get(BASE_API_URL+"/loadInitialData",(req,res)=>{
+
+    db.find({}, function(err,filteredList){
+
+        if(err){
+            res.sendStatus(500, "CLIENT ERROR");
+        
+        }
+        if(filteredList==0){
+            for(var i = 0; i<stats.length;i++){
+                db.insert(stats[i]);
+            }
+            res.sendStatus(200,"OK");
+            
+        }
+    });
+
+});
+
+//Metodo auxiliar
+
+function check_body(req){
+    return (req.body.countries == null |
+             req.body.years == null | 
+             req.body.appearences == null | 
+             req.body.cleanSheets == null | 
+             req.body.goals == null);
+};
+
+function pagination(req, lista){
+
+    var res = [];
+    const limit = req.query.limit;
+    const offset = req.query.offset;
+    
+    if(limit < 1 || offset < 0 || offset > lista.length){
+        res.push("ERROR EN PARAMETROS LIMIT Y/O OFFSET");
+        return res;
+    }
+
+    res = lista.slice(offset,parseInt(limit)+parseInt(offset));
+    return res;
+
+};
+
+
+
+//GETs
+
+//GET Global y años
+
+app.get(BASE_API_URL,(req,res)=>{
+    var years = req.query.years;
+    var from = req.query.from;
+    var to   = req.query.to;
+
+    //Comprobaciones
+    //Comprobacion query 
+
+    for(var i = 0; i<Object.keys(req.query).length;i++){
+        var element = Object.keys(req.query)[i];
+        if(element != "year" && element != "from" && element != "to" && element != "limit" && element != "offset"){
+            res.sendStatus(400, "BAD REQUEST");
+            
+        }
+    }
+    
+    //Comprobacion from menor que to
+
+    if(from>to){
+        res.sendStatus(400, "BAD REQUEST");
+        
+    }
+
+    db.find({},function(err, filteredList){
+
+        if(err){
+            res.sendStatus(500, "CLIENT ERROR");   
+        }
+
+        // Apartado para búsqueda por año
+        
+        if(years != null){
+            var filteredList = filteredList.filter((reg)=>
+            {
+                return (reg.years == years);
+            });
+            if (filteredList==0){
+                res.sendStatus(404, "NOT FOUND");     
+            }
+        }
+
+        // Apartado para from y to
+        
+        if(from != null && to != null){
+            filteredList = filteredList.filter((reg)=>
+            {
+                return (reg.years >= from && reg.years <=to);
+            });
+
+            if (filteredList==0){
+                res.sendStatus(404, "NOT FOUND");
+            }    
+
+            
+        }
+        // RESULTADO
+
+        if(req.query.limit != undefined || req.query.offset != undefined){
+            filteredList = pagination(req,filteredList);
+        }
+        filteredList.forEach((element)=>{
+            delete element._id;
+        });
+        res.send(JSON.stringify(filteredList,null,2));
+    });
+    
+  
+
+});
+
+//GET Pais y opcion entre años
+
+app.get(BASE_API_URL+"/:countries",(req, res)=>{
+
+    var countries =req.params.countries
+    var from = req.query.from;
+    var to = req.query.to;
+
+    //Comprobaciones
+    //Comprobaciones query
+    
+    for(var i = 0; i<Object.keys(req.query).length;i++){
+        var element = Object.keys(req.query)[i];
+        if(element != "from" && element != "to"){
+            res.sendStatus(400, "BAD REQUEST");
+            return;
+        }
+    }
+
+     //Comprobacion from menor que to
+
+     if(from>to){
+        res.sendStatus(400, "BAD REQUEST");
+        
+    }
+
+    db.find({}, function(err,filteredList){
+            
+        if(err){
+            res.sendStatus(500, "CLIENT ERROR");
+            return;
+        }
+
+        filteredList = filteredList.filter((reg)=>
+        {
+            return (reg.countries == countries);
+        });
+
+
+        if(from != null && to != null && from<=to){
+            filteredList = filteredList.filter((reg)=>
+            {
+               return (reg.years >= from && reg.years <=to);
+            }); 
+            
+        }
+        //COMPROBAMOS SI EXISTE
+        if (filteredList==0){
+            res.sendStatus(404, "NOT FOUND");
+            return;
+        }
+        //RESULTADO
+        if(req.query.limit != undefined || req.query.offset != undefined){
+            filteredList = pagination(req,filteredList);
+        }
+        filteredList.forEach((element)=>{
+            delete element._id;
+        });
+        res.send(JSON.stringify(filteredList,null,2));
+    });
+
+});
+
+
+//GET por Pais y Año
+
+app.get(BASE_API_URL+"/:countries/:years",(req, res)=>{
+
+    var countries =req.params.countries
+    var years = req.params.years
+
+    db.find({},function(err, filteredList){
+
+        if(err){
+            res.sendStatus(500, "ERROR EN CLIENTE");
+            
+        }
+
+        filteredList = filteredList.filter((reg)=>
+        {
+            return (reg.countries == countries && reg.years == years);
+        });
+        if (filteredList==0){
+            res.sendStatus(404, "NO EXISTE");
+            
+        }
+
+        //RESULTADO
+
+        //Paginación
+        if(req.query.limit != undefined || req.query.offset != undefined){
+            filteredList = pagination(req,filteredList);
+            res.send(JSON.stringify(filteredList,null,2));
+        }
+        filteredList.forEach((element)=>{
+            delete element._id;
+        });
+        res.send(JSON.stringify(filteredList[0],null,2));
+    });
+
+});
+
+//POSTs
+
+//POST resources list
+
+app.post(BASE_API_URL,(req, res)=>{
+    
+    //Comprobamos formato JSON
+    if(check_body(req)){
+        res.sendStatus(400,"BAD REQUEST - Parametros incorrectos");
+    }else{
+        db.find({},function(err,filteredList){
+
+            if(err){
+                res.sendStatus(500, "CLIENT ERROR");
+               
+            }
+
+            filteredList = filteredList.filter((reg)=>
+            {
+                return(req.body.countries == reg.countries && req.body.years == reg.years)
+            })
+        
+            if(filteredList.length != 0){
+                res.sendStatus(409,"CONFLICT");
+            }else{
+                db.insert(req.body);
+                res.sendStatus(201,"CREATED");
+            }
+        });
+    }
+
+});
+// POST para recurso concreto
+
+app.post(BASE_API_URL+"/:countries",(req, res)=>{
+    res.sendStatus(405,"METHOD NOT ALLOWED");
+});
+
+
+//PUTs
+
+// PUT de una lista de recursos
+
+app.put(BASE_API_URL,(req, res)=>{
+    
+    res.sendStatus(405,"METHOD NOT ALLOWED");
+});
+
+// PUT de un recurso especifico
+
+app.put(BASE_API_URL+"/:countries/:years",(req, res)=>{
+    
+    //Comprobamos formato JSON
+    if(check_body(req)){
+        res.sendStatus(400,"BAD REQUEST - Parametros incorrectos");
+    }
+    var countryR = req.params.countries;
+    var yearR = req.params.years;
+    var body = req.body;  
+
+    db.find({},function(err,filteredList){
+            if(err){
+                res.sendStatus(500, "CLIENT ERROR");
+                return;
+            }
+
+            //COMPROBAMOS SI EXISTE EL ELEMENTO
+
+            filteredList = filteredList.filter((reg)=>
+            {
+                return (reg.country == countryR && reg.year == yearR);
+            });
+            if (filteredList==0){
+                res.sendStatus(404, "NOT FOUND");
+                return;
+            }
+
+            //COMPROBAMOS SI LOS CAMPOS ACTUALIZADOS SON IGUALES
+
+            if(countryR != body.countries || yearR != body.years){
+                res.sendStatus(400,"BAD REQUEST");
+                return;
+            }
+
+            //ACTUALIZAMOS VALOR
+                
+            db.update({$and:[{countries: String(countryR)}, {years: parseInt(yearR)}]}, {$set: body}, {},function(err, numUpdated) {
+                if (err) {
+                    res.sendStatus(500, "CLIENT ERROR");
+                    return;
+                }else{
+                    res.sendStatus(200,"UPDATED");
+                    return;
+                }
+            });
+    });   
+    
+
+});
+
+// DELETEs
+
+// DELETE de una lista de recursos
+
+app.delete(BASE_API_URL,(req, res)=>{
+    db.remove({}, { multi: true }, (err, numRemoved)=>{
+        if (err){
+            res.sendStatus(500,"CLIENT ERROR");
+            
+        }
+        res.sendStatus(200,"DELETED");
+        
+    });
+});
+
+// DELETE de un recurso especifico
+
+app.delete(BASE_API_URL+"/:countries/:years",(req, res)=>{
+    var countryR = req.params.countries;
+    var yearR = req.params.years;
+    db.find({countries: countryR, years: parseInt(yearR)}, {}, (err, filteredList)=>{
+        if (err){
+            res.sendStatus(500,"ERROR EN CLIENTE");
+            return;
+        }
+        if(filteredList==0){
+            res.sendStatus(404,"NOT FOUND");
+            return;
+        }
+        db.remove({countries: countryR, years: yearR}, {}, (err, numRemoved)=>{
+            if (err){
+                res.sendStatus(500,"ERROR EN CLIENTE");
+                return;
+            }
+        
+            res.sendStatus(200,"DELETED");
+            return;
+            
+        });
+    });
+});
+
+};
---
+++ src/back/premier-league-stats/v2/premier-league.js
@@ -0,0 +1,256 @@
+var Datastore = require("nedb");
+var db = new Datastore();
+var BASE_API_PATH = "/api/v2/premier-league"; 
+
+var defenseStats = [];
+
+
+module.exports.register = (app) => {
+    
+    //Portal de Documentacion
+
+    app.get(BASE_API_PATH+"/docs",(req,res)=>
+    {
+        res.redirect("https://documenter.getpostman.com/view/19586040/UVyn2yuS");
+    });
+
+    db.insert(defenseStats);
+
+    //Constructor
+
+     //GET inicial (loadInitialData) para inicializar la BD (constructor)
+
+     app.get(BASE_API_PATH+"/loadInitialData",(req,res)=>{
+
+        var statsIni = [
+            {
+                countries:"England",
+                years:2018,
+                appearences:38,
+                cleanSheets:21,
+                goals:22
+            },
+            {
+                countries:"England",
+                years:2019,
+                appearences:38,
+                cleanSheets:16,
+                goals:21
+            },
+            {
+                countries:"Belgium",
+                years:2015,
+                appearences:38,
+                cleanSheets:11,
+                goals:18
+            },
+            {
+                countries:"Belgium",
+                years:2017,
+                appearences:38,
+                cleanSheets:16,
+                goals:16
+            },
+            {
+                countries:"Spain",
+                years:2013,
+                appearences:37,
+                cleanSheets:12,
+                goals:9
+            },
+            {
+                countries:"Spain",
+                years:2020,
+                appearences:37,
+                cleanSheets:10,
+                goals:7
+            }
+        ];
+
+        // Inicialización base de datos
+        //Borra todo lo anterior para evitar duplicidades al hacer loadInitialData
+        db.remove({}, { multi: true }, function (err, numRemoved) {
+        });
+
+        // Inserta los datos iniciales en la base de datos
+        db.insert(statsIni);
+
+        res.send(JSON.stringify(statsIni,null,2));
+
+
+    });
+
+    /*------------------- GETs -------------------*/
+
+
+     //GET A UNA LISTA DE RECURSOS DE PREMIER-LEAGUE-STATS
+    app.get(BASE_API_PATH, (req,res)=>{
+        var query = req.query;
+        var limit = parseInt(query.limit);
+        var offset = parseInt(query.offset);
+        var dbquery = {};
+
+        //"Parseamos" los datos a su tipo original antes de buscar
+        if (req.query.countries) dbquery["countries"] = req.query.countries;
+        if (req.query.years) dbquery["years"] = parseInt(req.query.years);
+        if (req.query.appearences) dbquery["appearences"] = parseFloat(req.query.appearences);
+        if (req.query.cleanSheets) dbquery["cleanSheets"] = parseFloat(req.query.cleanSheets);
+        if (req.query.goals) dbquery["goals"] = parseFloat(req.query.goals);
+
+        //Búsqueda de datos y ordenación por parametro country
+        db.find(dbquery).sort({countries:1, years:-1}).skip(offset).limit(limit).exec((error, dataPremier) => {
+            if (error){
+                console.error("Error accessing DB in POST: " + err);
+                res.sendStatus(500);
+            }else {
+
+            //Se elimina el _id creado automáticamente
+            dataPremier.forEach((t) => {
+                delete t._id
+            });
+
+            res.send(JSON.stringify(dataPremier, null, 2));
+            console.log("GET REQUEST have been sent.");
+            }
+        });
+    });
+
+
+     //GET A UN RECURSO CONCRETO DE SMOKER POR COUNTRY/YEAR    
+     app.get(BASE_API_PATH+"/:countries/:years", (req, res) => {
+        var reqCountry = req.params.countries;
+        var reqYear = parseInt(req.params.years);
+
+        db.find({ countries: reqCountry, years: reqYear }, { _id: 0 }, function (err, data) {
+            if (err) {
+                console.error("ERROR in GET: "+err);
+                res.sendStatus(500);
+            } else {
+                if(data.length != 0){                
+                console.log(`NEW GET request to <${reqCountry}>, <${reqYear}>`);
+                res.status(200).send(JSON.stringify(data,null,2));
+                }else{
+                    console.error("Data not found");
+                    res.status(404).send("Data not found in DB.");
+                }
+
+            }
+        });
+    });
+
+    /*------------------- POSTs -------------------*/
+
+
+    //POST A LA LISTA DE RECURSOS DE PREMIER-LEAGUE-STATS 
+    app.post(BASE_API_PATH,(req,res)=>{
+        var dataNew = req.body;
+        var countryNew = req.body.countries;
+        var yearNew = req.body.years;
+        
+        
+        db.find({ countries: countryNew, years: yearNew }, (err, data) => {
+            if (err) {
+                console.error("Error accessing DB in POST: " + err);
+                res.sendStatus(500);
+            } else {
+                if (data.length == 0) {
+                    if (!dataNew.countries ||
+                        !dataNew.years ||
+                        !dataNew.appearences ||
+                        !dataNew.cleanSheets ||
+                        !dataNew.goals) {
+                        console.log("Number of parameters is incorrect.");
+                        return res.status(400).send("Format incorrect.");
+                    }else {
+                        console.log("Inserting new data in DB: " + JSON.stringify(dataNew, null, 2));
+                        db.insert(dataNew);
+                        return res.status(201).send("Se ha creado correctamente: " +JSON.stringify(dataNew, null, 2));
+                    }
+                } else {
+                    console.log("Conflit is detected.");
+                    res.sendStatus(409);
+                }
+            }
+        });
+    });
+
+     //POST A UN RECURSO DE DEFENSE (No está permitido)
+     app.post(BASE_API_PATH+"/:countries/:years",(req,res)=>{
+        res.sendStatus(405);
+        console.log("Se ha intentado hacer POST a un recurso concreto.");
+    });
+
+    /*------------------- PUTs -------------------*/
+
+     //PUT A UN RECURSO CONCRETO DE DEFENSE POR COUNTRY/YEAR
+     app.put(BASE_API_PATH+"/:countries/:years", (req,res) => {
+        
+        var reqcountry = req.params.countries;
+        var reqyear = parseInt(req.params.years);
+        var data = req.body;
+
+        if (Object.keys(data).length != 7) {
+            console.log("Actualizacion de campos no valida");
+            res.sendStatus(400);
+        }else {
+            db.update({ countries: reqcountry, years: reqyear }, { $set: data }, {}, function (err, dataUpdate) {
+                if (err) {
+                    console.error("ERROR accesing DB in GET");
+                    res.sendStatus(500);
+                } else {
+                    if (dataUpdate == 0) {
+                        console.error("No data found");
+                        res.sendStatus(404);
+                    } else {
+                        console.log("Campos actualizados")
+                        res.sendStatus(200);
+                    }
+                }
+            });
+        }
+    });
+
+     //PUT A UNA LISTA DE RECURSOS DE DEFENSEs STATS (Debe dar error)
+     app.put(BASE_API_PATH,(req,res) => {
+        res.sendStatus(405);
+    });
+
+
+
+    /*------------------- DELETEs -------------------*/
+
+
+     //DELETE A LISTA DE RECURSOS DE DEFENSE STATS
+     app.delete(BASE_API_PATH, (req,res) => {
+        db.remove({}, {multi: true}, (err, numDataRemoved) => {
+            if (err || numDataRemoved == 0){
+                console.log("ERROR deleting DB: "+err);
+                res.sendStatus(500);
+            }else{
+                console.log(numDataRemoved+" has been successfully deleted from the BD.");
+                res.sendStatus(200);
+            }
+        });
+    });
+
+    //DELETE A UN RECURSO DE DEFENSE POR COUNTRY/YEAR
+    app.delete(BASE_API_PATH + "/:countries/:years", (req,res)=>{
+        var reqcountry = req.params.countries;
+        var reqyear = parseInt(req.params.years);
+        db.remove({countries : reqcountry, years : reqyear},{multi:true}, (err, data) => {
+            if (err) {
+                console.error("ERROR in GET");
+                res.sendStatus(500);
+            } else {
+                if(data != 0){
+                    console.log(`NEW DELETE request to <${reqcountry}>, <${reqyear}>`);
+                    res.status(200).send("The corresponding data for " + reqcountry + " and " + reqyear + " has been deleted");
+                }else{
+                    console.log("Data not found");
+                    res.sendStatus(404);
+                }
+            }
+        });
+    });
+
+};
