30092e5ae6c8ba1ade9e1b672f9717aad34cf92f
Antonio Saborido <antonio.saborido01@gmail.com>
2022-05-18 18:49:16 +0200

    herokaso parent3

--- src/front/tennis/twitch.svelte
+++ src/front/tennis/twitch.svelte
@@ -55,10 +55,10 @@ loading
 			{#each entries as entry}
 				<tr>
 					<td>{entry.title}</td>
-					<td>{entry.n}</td>
+					<td>{entry.view_count}</td>
                     <td>
 						<iframe
-   src="https://clips.twitch.tv/embed?clip={entry.id}&parent=http://sos2122-23.herokuapp.com"
+   src="https://clips.twitch.tv/embed?clip={entry.id}&parent=sos2122-23.herokuapp.com"
    height="360"
    width="640"
    allowfullscreen>
