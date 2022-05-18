68f6dc3052438b908d5276a8bd9ede422bc1fc4e
Antonio Saborido <antonio.saborido01@gmail.com>
2022-05-18 18:36:19 +0200

    222

--- src/front/Home.svelte
+++ src/front/Home.svelte
@@ -12,11 +12,41 @@
         <h2>Project description:</h2> Relación de deportes entre fútbol (Premier League), tenis y baloncesto (NBA)
         <h2>Repository:</h2><a href="https://github.com/gti-sos/SOS2122-23">gti-sos/SOS2122-23</a>
         <h2>URL:</h2><a href="http://sos2122-23.herokuapp.com"> http://sos2122-23.herokuapp.com</a>
-        <h2>APIS:</h2>
-        <li><a href="https://sos2122-23.herokuapp.com/api/v2/premier-league">https://sos2122-23.herokuapp.com/api/v1/premier-league-stats (developed by Alberto Martin Martin)(https://github.com/albmarmar6)</a></li>
-        <li><a href="https://sos2122-23.herokuapp.com/api/v1/nba-stats">https://sos2122-23.herokuapp.com/api/v1/nba-stats (developed by Fernando Pardo Beltrán)</a></li>
+        <hr>
+        <h4>DOCS</h4>
+        <li><a href="https://sos2122-23.herokuapp.com/api/v1/tennis/docs">https://sos2122-23.herokuapp.com/api/v1/tennis/docs </a> </li> 
+        <li><a href="https://sos2122-23.herokuapp.com/api/v2/tennis/docs">https://sos2122-23.herokuapp.com/api/v2/tennis/docs </a> </li> 
+        <h4>API</h4>
+        <li><a href="https://sos2122-23.herokuapp.com/api/v1/tennis">https://sos2122-23.herokuapp.com/api/v1/tennis </a> </li> 
+        <li><a href="https://sos2122-23.herokuapp.com/api/v2/tennis">https://sos2122-23.herokuapp.com/api/v2/tennis </a> </li> 
         
-        <li><a href="https://sos2122-23.herokuapp.com/api/v1/tennis/docs">https://sos2122-23.herokuapp.com/api/v1/tennis/docs </a> (developed by Antonio Saborido Campos)</li> 
+        <h4>FRONT END</h4>
+        <li><a href="https://sos2122-23.herokuapp.com/#/tennis">https://sos2122-23.herokuapp.com/#/tennis </a> </li> 
+        <p>(developed by [Antonio Saborido Campos](https://github.com/Antoniiosc7)</p>
+        <hr>
+
+        <h4>DOCS</h4>
+        <li><a href="https://sos2122-23.herokuapp.com/api/v1/premier-league/docs">https://sos2122-23.herokuapp.com/api/v1/premier-league/docs </a> </li> 
+        <li><a href="https://sos2122-23.herokuapp.com/api/v2/premier-league/docs">https://sos2122-23.herokuapp.com/api/v2/premier-league/docs </a> </li> 
+        <h4>API</h4>
+        <li><a href="https://sos2122-23.herokuapp.com/api/v1/premier-league">https://sos2122-23.herokuapp.com/api/v1/premier-league </a> </li> 
+        <li><a href="https://sos2122-23.herokuapp.com/api/v2/premier-league">https://sos2122-23.herokuapp.com/api/v2/premier-league </a> </li> 
+        
+        <h4>FRONT END</h4>
+        <li><a href="https://sos2122-23.herokuapp.com/#/premier-league">https://sos2122-23.herokuapp.com/#/premier-league </a> </li> 
+        <p>(developed by [Alberto Martín Martín](https://github.com/albmarmar6)</p>
+        <hr>
+
+        <h4>DOCS</h4>
+        <li><a href="https://sos2122-23.herokuapp.com/api/v1/nba-stats/docs">https://sos2122-23.herokuapp.com/api/v1/nba-stats/docs </a> </li> 
+        <li><a href="https://sos2122-23.herokuapp.com/api/v2/nba-stats/docs">https://sos2122-23.herokuapp.com/api/v2/nba-stats/docs </a> </li> 
+        <h4>API</h4>
+        <li><a href="https://sos2122-23.herokuapp.com/api/v1/nba-stats">https://sos2122-23.herokuapp.com/api/v1/nba-stats </a> </li> 
+        <li><a href="https://sos2122-23.herokuapp.com/api/v2/nba-stats">https://sos2122-23.herokuapp.com/api/v2/nba-stats </a> </li> 
+        
+        <h4>FRONT END</h4>
+        <li><a href="https://sos2122-23.herokuapp.com/#/nba-stats">https://sos2122-23.herokuapp.com/#/nba-stats </a> </li> 
+        <p>(developed by [Fernando Pardo Beltrán](https://github.com/Nando13-hub)</p>
     </div>
         
 </main>
