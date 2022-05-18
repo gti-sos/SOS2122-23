b1c4c469318f991c26668bc65d415954c10e6fe0
Antonio Saborido <antonio.saborido01@gmail.com>
2022-05-18 16:39:53 +0200

    header act

--- src/front/components/Header.svelte
+++ src/front/components/Header.svelte
@@ -1,21 +1,74 @@
 <header>
-    <a href="">
-         <h1>
-        Inicio
-        </h1>
-    </a>
+    <div class="container">
+        <div id="titulo">
+            <a href=""><h2>INICIO</h2></a>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <a href="/#/about"><h5>About</h5></a>
+            </div>
+            <div class="col">
+                <a href="/#/analytics"><h5>Analytics</h5></a>
+            </div>
+            <div class="col">
+                <a href="/#/integrations"><h5>Integrations</h5></a>
+            </div>
+            <div class="col">
+                <a href="/#/info"><h5>Info</h5></a>
+            </div>
+            <div class="col">
+                <a href="/#/groupgraph"><h5>GroupGraph</h5></a>
+            </div>
+            `
+            <div class="col" id="negro">|</div>
+            <div class="col">
+                <a href="/#/premier-league"><h5>PremierLeague</h5></a>
+            </div>
+            <div class="col">
+                <a href="/#/tennis"><h5>Tennis</h5></a>
+            </div>
+            <div class="col">
+                <a href="/#/nba-stats"><h5>NBA</h5></a>
+            </div>
+        </div>
+    </div>
 </header>
 
 <style>
-    header{
-        background: #f7f7f7;
-        padding: 20px;
+    a {
+        color: black;
     }
-    h1{
-        margin:0;
+    a:hover {
+        color: blue;
+    }
+    #negro{
+        color:black;
+    }
+
+    .row {
+        padding: 2px;
+        color: white;
+    }
+
+    .container {
+        padding: 2px 8px;
+        text-align: center;
+
+    }
+    header {
+        background: #f7f7f7;
+        padding: 10px;
+    }
+    h1 {
+        margin: 0;
         text-align: center;
     }
-    img{
+    h5 {
+        margin: 0;
+        text-align: center;
+    }
+    img {
         max-width: 180px;
     }
 </style>
