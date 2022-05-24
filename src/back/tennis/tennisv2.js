592fd3d8d05c993f23d5e5a73d07d6d3fec99843
Antonio Saborido <antonio.saborido01@gmail.com>
2022-05-25 01:14:27 +0200

    restore 2

--- src/back/tennis/tennisv2.js
+++ src/back/tennis/tennisv2.js
@@ -3,23 +3,7 @@ const express = require("express");
 const app = express();
 const BASI_API_TENNIS2 = "/api/v2/tennis";
 //API Antonio Saborido
-//Proxy
 
-var paths='/remoteAPI';
-var apiServerHost = 'https://sos2122-23.herokuapp.com/api/v2/tennis';
-
-app.use(paths, function(req, res) {
-  var url = apiServerHost + req.url;
-  console.log('piped: ' + req.url);
-  req.pipe(request(url)).pipe(res);
-});
-
-var remoteAPI1 = "http://api.quotable.io/random"
-var pathQuote = "/random"
-app.use(pathQuote, function(req,res){
-	console.log("Piped:" + req.baseUrl + req.url);
-	req.pipe(request(remoteAPI1)).pipe(res);
-});
 
 
     // ########################################################### //
