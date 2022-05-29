76500aeef6e06ccc0b6856ad702a3a463972de8d
Antonio Saborido <antonio.saborido01@gmail.com>
2022-05-29 23:10:29 +0200

    the last change

--- src/back/tennis/tennisv2.js
+++ src/back/tennis/tennisv2.js
@@ -3,23 +3,6 @@ const express = require("express");
 const app = express();
 const BASI_API_TENNIS2 = "/api/v2/tennis";
 //API Antonio Saborido
-//Proxy
-
-var paths='/remoteAPI';
-var apiServerHost = 'https://sos2122-23.herokuapp.com/api/v2/tennis';
-
-app.use(paths, function(req, res) {
-  var url = apiServerHost + req.url;
-  console.log('piped: ' + req.url);
-  req.pipe(request(url)).pipe(res);
-});
-
-var remoteAPI1 = "http://api.quotable.io/random"
-var pathQuote = "/random"
-app.use(pathQuote, function(req,res){
-	console.log("Piped:" + req.baseUrl + req.url);
-	req.pipe(request(remoteAPI1)).pipe(res);
-});
 
 
     // ########################################################### //
@@ -538,12 +521,16 @@ module.exports.register = (app,db) =>{
     
     function comprobar_body(req){
         return (req.body.country == null |
+                isNaN(req.body.year) |
                  req.body.year == null | 
                  req.body.most_grand_slam == null | 
+                 isNaN(req.body.most_grand_slam) |
                  req.body.masters_finals == null | 
-                 req.body.olympic_gold_medals == null);
+                 isNaN(req.body.masters_finals) |
+                 isNaN(req.body.olympic_gold_medals) |
+                 req.body.olympic_gold_medals  == null);
     }
-
+    
     function paginacion(req, lista){
 
         var res = [];
